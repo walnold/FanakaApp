d5b809391aec4e1e13c39b64557c258464b40d32
Walter Ndung'u <Walterbilionnaire@gmail.com>
2026-03-27 12:54:48 +0300

    add auth class for learner

--- backend/learner/views/learner.py
+++ backend/learner/views/learner.py
@@ -7,24 +7,29 @@ from learner.serializers.enrollement import  EnrollmentSerializer
 from learner.serializers.learner import LearnerSerializer
 from learner.serializers.payment import PaymentSerializer
 from learner.serializers.lessons import LessonSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class LearnerViewSet(viewsets.ModelViewSet):
     queryset = Learner.objects.all()
     serializer_class = LearnerSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication] 
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
     queryset = Enrollement.objects.all()
     serializer_class = EnrollmentSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication] 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payments.objects.all()
     serializer_class = PaymentSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication] 
 
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication] 
