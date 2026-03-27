3303f086f71f84fa0b8bd8405a733d016d6466c8
Walter Ndung'u <Walterbilionnaire@gmail.com>
2026-03-27 12:42:07 +0300

    update Default authenticaiton class

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -141,6 +141,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication', # For Bearer tokens
+        'rest_framework.authentication.SessionAuthentication',      # Optional: for browsable API
+    ),
 }
 
 # Internationalization
